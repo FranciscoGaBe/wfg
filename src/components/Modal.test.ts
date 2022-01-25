1bbc20058c500cf5cc828049aca04ba19308710b
Francisco Garrido <francisco.garrido.bear@gmail.com>
2022-01-25 05:06:28 +0100

    Feat: Added Modal component

---
+++ src/components/Modal.test.ts
@@ -0,0 +1,79 @@
+import { mount } from '@vue/test-utils'
+import Modal from './Modal.vue'
+
+describe('Modal', () => {
+
+  let wrapper = mount(Modal)
+
+  beforeEach(() => {
+    wrapper = mount(Modal)
+  })
+
+  it('has a show prop that defaults to false', () => {
+
+    expect(wrapper.props().show).toBe(false)
+
+  })
+
+  it('has display none when "show" is set false', async () => {
+
+    expect.assertions(2)
+    expect((<HTMLElement>wrapper.element).style.display).toBe('none')
+    await wrapper.setProps({
+      show: true
+    })
+    expect((<HTMLElement>wrapper.element).style.display).not.toBe('none')
+
+  })
+
+  it('has a title prop to display', async () => {
+
+    expect.assertions(4)
+    expect(wrapper.props().title).toBe('')
+    await wrapper.setProps({
+      title: 'Test'
+    })
+    expect(wrapper.props().title).toBe('Test')
+    const title = wrapper.find('.modal-title')
+    expect(title.exists()).toBe(true)
+    expect(title.text()).toBe('Test')
+
+  })
+
+  it('has a button to close itself', async () => {
+
+    expect.assertions(2)
+    await wrapper.setProps({
+      show: true
+    })
+    const button = wrapper.find('.modal-close')
+    expect(button.exists()).toBe(true)
+    await button.trigger('click')
+    expect(wrapper.emitted('update:show')).toBeTruthy()
+
+  })
+
+  it('has a slot for content', () => {
+
+    wrapper = mount(Modal, {
+      slots: {
+        default: 'Content'
+      }
+    })
+
+    const content = wrapper.find('.modal-content')
+    expect(content.text()).toBe('Content')
+
+  })
+
+  it('closes when clicking outside the window', async () => {
+
+    expect.assertions(2)
+    await wrapper.find('.modal-content').trigger('click')
+    expect(wrapper.emitted('update:show')).toBeFalsy()
+    await wrapper.trigger('click')
+    expect(wrapper.emitted('update:show')).toBeTruthy()
+
+  })
+
+})
---
+++ src/components/Modal.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+
+interface Props {
+  show?: boolean,
+  title?: string
+}
+
+withDefaults(defineProps<Props>(), { show: false, title: '' })
+
+const emits = defineEmits<{
+  (e: 'update:show', show: boolean): void
+}>()
+
+const close = () => {
+
+  emits('update:show', false)
+
+}
+
+</script>
+
+<template>
+  <div v-show="show" class="fixed inset-0 flex items-center justify-center" @click.self="close">
+    <div
+      class="bg-gray-100 max-w-full w-96 max-h-full h-96 rounded shadow animate-[bounce_0.5s_linear_0s_0.5]"
+    >
+      <div
+        class="flex items-center justify-between pl-4 py-2 pr-2 border-b-2 border-rose-300 shadow"
+      >
+        <div class="modal-title font-semibold text-xl">{{ title }}</div>
+        <button class="modal-close material-icons" @click="close">close</button>
+      </div>
+      <div class="modal-content p-4">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
