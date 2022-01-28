6da07be9268bd8191462d1e863792e23aad18569
Francisco Garrido <francisco.garrido.bear@gmail.com>
2022-01-28 16:49:39 +0100

    Style: fixed styling on small devices

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -20,20 +20,22 @@ const close = () => { emits('update:show', false) }
     <div
       v-show="show"
       ref="modalRef"
-      class="modal fixed -inset-8 flex items-center justify-center z-50 bg-black bg-opacity-70 p-1"
+      class="modal fixed -inset-8 flex items-center justify-center z-50 bg-black bg-opacity-70"
       @click.self="close"
     >
-      <div ref="bodyRef" class="bg-gray-800 max-w-full min-w-96 max-h-full rounded shadow">
-        <div
-          class="flex items-center justify-between pl-4 py-2 pr-2 border-b-2 border-rose-600 shadow shadow-rose-600/50"
-        >
-          <div class="modal-title font-bold text-3xl text-rose-700">{{ title }}</div>
-          <button class="modal-close flex items-center justify-center" @click="close">
-            <i class="material-icons !text-3xl text-rose-600 !font-bold">close</i>
-          </button>
-        </div>
-        <div class="modal-content p-4 text-rose-500 font-medium">
-          <slot></slot>
+      <div class="p-1 max-w-[100vw] w-96 max-h-screen h-full md:h-auto">
+        <div ref="bodyRef" class="bg-gray-800 w-full h-full rounded shadow flex flex-col">
+          <div
+            class="flex-shrink flex items-center justify-between pl-4 py-2 pr-2 border-b-2 border-rose-600 shadow shadow-rose-600/50"
+          >
+            <div class="modal-title font-bold text-3xl text-rose-700">{{ title }}</div>
+            <button class="modal-close flex items-center justify-center" @click="close">
+              <i class="material-icons !text-3xl text-rose-600 !font-bold">close</i>
+            </button>
+          </div>
+          <div class="modal-content flex-grow p-4 text-rose-500 font-medium overflow-auto">
+            <slot></slot>
+          </div>
         </div>
       </div>
     </div>
